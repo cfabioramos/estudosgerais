5456667e49b94e5ca418092f2ceae2de855ea3ed
Carlos Fábio <cfabioramos@gmail.com>
2018-08-27 21:52:56 -0300

    initial files

---
+++ EstudoJava/src/Abstrata.java
@@ -0,0 +1,4 @@
+
+public abstract class Abstrata extends Solution{
+
+}
---
+++ EstudoJava/src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		
+		//TODO fazer isso.
+		List<Integer> mySet = Arrays.asList(8, 9, 6, 37);
+		int smallest = Collections.min(mySet);
+		System.out.println(smallest);
+    }
+	
+	static int doStuff(String arg) {
+		return Integer.parseInt(arg);
+	}
+
+}
